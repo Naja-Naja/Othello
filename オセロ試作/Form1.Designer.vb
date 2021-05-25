6942551b3769dacc503f355f6c914a63cac2ea90
Naja-Naja <latimeria.h11@gmail.com>
2021-05-25 21:17:11 +0900

    初回コミット

---
+++ オセロ試作/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Othello_Field = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Start_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        CType(Me.Othello_Field, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Othello_Field
+        '
+        Me.Othello_Field.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Othello_Field.Location = New System.Drawing.Point(33, 24)
+        Me.Othello_Field.Name = "Othello_Field"
+        Me.Othello_Field.Size = New System.Drawing.Size(400, 400)
+        Me.Othello_Field.TabIndex = 0
+        Me.Othello_Field.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Start_Button
+        '
+        Me.Start_Button.Location = New System.Drawing.Point(503, 303)
+        Me.Start_Button.Name = "Start_Button"
+        Me.Start_Button.Size = New System.Drawing.Size(129, 48)
+        Me.Start_Button.TabIndex = 1
+        Me.Start_Button.Text = "Start"
+        Me.Start_Button.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(11, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(11, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(11, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 199)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(11, 12)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 241)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(11, 12)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 294)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(11, 12)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 348)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(11, 12)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 396)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(11, 12)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(400, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(11, 12)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "8"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(352, 9)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(11, 12)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "7"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(298, 9)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(11, 12)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "6"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(245, 9)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(11, 12)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "5"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(197, 9)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(11, 12)
+        Me.Label13.TabIndex = 13
+        Me.Label13.Text = "4"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(140, 9)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(11, 12)
+        Me.Label14.TabIndex = 12
+        Me.Label14.Text = "3"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(97, 9)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(11, 12)
+        Me.Label15.TabIndex = 11
+        Me.Label15.Text = "2"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(53, 9)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(11, 12)
+        Me.Label16.TabIndex = 10
+        Me.Label16.Text = "1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(680, 450)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Start_Button)
+        Me.Controls.Add(Me.Othello_Field)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Othello_Field, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Othello_Field As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Start_Button As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+End Class
---
+++ オセロ試作/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+    Dim Field(7, 7) As Integer '盤面全体の状況を配列でもつ。0：空白　1：黒　-1：白
+    Dim PlayerTurn As Boolean = True '現在どちらのターンかを記録する
+    'スタートボタンの処理
+    Private Sub Start_Button_Click(sender As Object, e As EventArgs) Handles Start_Button.Click
+        FirstSetting()
+    End Sub
+    '盤面をリセットし、中心の４マスに石を並べる
+    Private Sub FirstSetting()
+        For x = 0 To 7 Step 1
+            For y = 0 To 7 Step 1
+                Field(x, y) = 0
+            Next
+        Next
+        Refresh()
+        Create_Grid()
+        Field(4, 3) = 1
+        Field(3, 4) = 1
+        Field(3, 3) = -1
+        Field(4, 4) = -1
+    End Sub
+    '盤面に線を描画する
+    Private Sub Create_Grid()
+        Dim grph As System.Drawing.Graphics = Othello_Field.CreateGraphics
+        Dim blackPen As New Pen(Color.Black, 10)
+        grph.DrawLine(blackPen, 0, 0, 0, 400)
+        grph.DrawLine(blackPen, 0, 0, 400, 0)
+        grph.DrawLine(blackPen, 400, 400, 400, 0)
+        grph.DrawLine(blackPen, 400, 400, 0, 400)
+        For x = 0 To 400 Step 50
+            grph.DrawLine(Pens.Black, x, 0, x, 400)
+            grph.DrawLine(Pens.Black, 0, x, 400, x)
+        Next
+    End Sub
+    '毎フレーム盤面を更新する
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Create_Field()
+    End Sub
+    '盤面に石を描画する
+    Private Sub Create_Field()
+        Dim grph As System.Drawing.Graphics = Othello_Field.CreateGraphics
+        For x = 0 To 7 Step 1
+            For y = 0 To 7 Step 1
+                If Field(x, y) > 0 Then
+                    grph.FillEllipse(Brushes.Black, x * 50, y * 50, 50, 50)
+                ElseIf Field(x, y) < 0 Then
+                    grph.FillEllipse(Brushes.White, x * 50, y * 50, 50, 50)
+                End If
+            Next
+        Next
+    End Sub
+    '石を置いた座標を得て黒石を置く
+    Private Sub Othello_Field_Click(sender As Object, e As MouseEventArgs) Handles Othello_Field.MouseDown, Othello_Field.Click
+        If PlayerTurn = False Then
+            Return
+        End If
+        Dim grph As System.Drawing.Graphics = Othello_Field.CreateGraphics
+        Dim x, y As Single
+        x = Math.Floor((e.X) / 50)
+        y = Math.Floor((e.Y) / 50)
+
+        If Turn_Stone_Simulator(x, y, 1) = False Then
+            Undo_Simulator()
+            Debug.WriteLine("Undo")
+        ElseIf Math.Abs(Field(x, y)) > 0 Then
+            Undo_Simulator()
+        Else
+            Field(x, y) = 1
+            grph.FillEllipse(Brushes.Black, x * 50, y * 50, 50, 50)
+            Turn_Stone()
+            PlayerTurn = False
+        End If
+    End Sub
+
+    '石を置いた座標を得て白石を置く
+    Private Sub White_Othello_Field_Click(sender As Object, e As MouseEventArgs) Handles Othello_Field.MouseDown
+        If PlayerTurn = True Then
+            Return
+        End If
+        Dim grph As System.Drawing.Graphics = Othello_Field.CreateGraphics
+        Dim x, y As Single
+        x = Math.Floor((e.X) / 50)
+        y = Math.Floor((e.Y) / 50)
+
+        If Turn_Stone_Simulator(x, y, -1) = False Then
+            Undo_Simulator()
+            Debug.WriteLine("Undo")
+        ElseIf Math.Abs(Field(x, y)) > 0 Then
+            Undo_Simulator()
+        Else
+            Field(x, y) = -1
+            grph.FillEllipse(Brushes.White, x * 50, y * 50, 50, 50)
+            Turn_Stone()
+            PlayerTurn = True
+        End If
+    End Sub
+
+    '配列のうちひっくり返せるマスの数値を二倍する関数。呼んだならばUndo_Simulator関数かTurn_Stone関数を必ず呼ぶ。ひっくり返せる石がある場合はTrueを返す
+    Private Function Turn_Stone_Simulator(x As Single, y As Single, Optional z As Integer = 100) As Boolean
+        Dim keypoint As Integer
+        Dim dx, dy As Integer
+        Dim serch_x, serch_y As Integer
+        Dim result As Integer = 1
+        Dim isTurn As Boolean = True
+        Dim a, b As Integer
+        If z = 100 Then
+            keypoint = Field(x, y)
+        Else
+            keypoint = z
+        End If
+
+        For dx = -1 To 1 Step 1
+            For dy = -1 To 1 Step 1
+                result = 1
+                serch_x = x
+                serch_y = y
+                While result = 1
+                    serch_x = serch_x + dx
+                    serch_y = serch_y + dy
+                    result = isTurn_Stone(keypoint, serch_x, serch_y)
+                    If result = 0 Then
+                        While Math.Abs(Field(serch_x - dx, serch_y - dy)) = 2
+                            serch_x = serch_x - dx
+                            serch_y = serch_y - dy
+                            Field(serch_x, serch_y) = Field(serch_x, serch_y) / 2
+
+                        End While
+                    End If
+                End While
+            Next
+        Next
+        isTurn = False
+        For a = 0 To 7 Step 1
+            For b = 0 To 7 Step 1
+                If Math.Abs(Field(a, b)) > 1 Then
+                    isTurn = True
+                End If
+            Next
+        Next
+        Return isTurn
+    End Function
+    '絶対値で2の入っているマスの数値を半分にする
+    Private Sub Undo_Simulator()
+        For a = 0 To 7 Step 1
+            For b = 0 To 7 Step 1
+                If Math.Abs(Field(a, b)) > 1 Then
+                    Field(a, b) = (Field(a, b) / 2)
+                End If
+            Next
+        Next
+    End Sub
+    '絶対値で2の入っているマスの色を変えて数値を半分にする
+    Private Sub Turn_Stone()
+        For a = 0 To 7 Step 1
+            For b = 0 To 7 Step 1
+                If Math.Abs(Field(a, b)) > 1 Then
+                    Field(a, b) = (Field(a, b) / 2) * -1
+                End If
+            Next
+        Next
+    End Sub
+    '指定された座標のマスとkeypointのマスを比較する。空、枠外：0　異色：１　同色：２
+    Private Function isTurn_Stone(keypoint As Integer, x As Integer, y As Integer) As Integer
+        If x >= 8 Or y >= 8 Or x <= -1 Or y <= -1 Then
+            Return 0
+        ElseIf Field(x, y) = 0 Then
+            Return 0
+        ElseIf keypoint = Field(x, y) * -1 Then
+            Field(x, y) = Field(x, y) * 2
+            Return 1
+        ElseIf keypoint = Field(x, y) Then
+            Return 2
+        End If
+        Return 0
+    End Function
+End Class
